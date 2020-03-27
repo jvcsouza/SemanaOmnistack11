3f5da06c4eb0659ec4e99d09c4f2fe37652fd43e
João Victor Souza <joao.victor.souza99@hotmail.com>
2020-03-26 22:16:00 -0300

    backend

---
+++ backend/src/routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const routes = express.Router();
+
+const OngController = require("./controllers/OngController");
+const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
+
+routes.post("/session", SessionController.create);
+
+routes.post("/ongs", OngController.store);
+routes.get("/ongs", OngController.index);
+
+routes.post("/incidents", IncidentController.store);
+routes.get("/incidents", IncidentController.index);
+routes.delete("/incidents/:id", IncidentController.delete);
+
+routes.use("/profile", ProfileController.validate);
+routes.get("/profile", ProfileController.index);
+
+module.exports = routes;
